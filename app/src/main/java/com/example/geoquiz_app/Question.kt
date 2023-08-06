ca232bde40177e4c415bfe770051cb19421f238b
Darkri-X <108106508+Darkri-X@users.noreply.github.com>
2023-08-06 12:18:22 +1000

    Added comments and documentation for clarity.

--- app/src/main/java/com/example/geoquiz_app/Question.kt
+++ app/src/main/java/com/example/geoquiz_app/Question.kt
@@ -2,4 +2,9 @@ package com.example.geoquiz_app
 
 import androidx.annotation.StringRes
 
+/**
+ * Represents a question in the GeoQuiz app.
+ * @param textResId The resource ID of the question text.
+ * @param answer The correct answer to the question (true for correct, false for incorrect).
+ */
 data class Question(@StringRes val textResId: Int, val answer: Boolean)
